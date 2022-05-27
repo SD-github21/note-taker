cb7f40250aacd31e9a4a0e06233354b475e4fb5c
SD-github21 <sapandonde.gmail.com>
2022-05-26 22:32:27 -0500

    set up routes directory structure and create GET and POST requests

--- server.js
+++ server.js
@@ -1,5 +1,33 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
+const PORT = process.env.PORT || 3001;
 const app = express();
+// const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
+const notes = require("./db/db.json");
+
+
+// Add middleware to read posted/incoming data
+// Parse incoming string or array data
+app.use(express.urlencoded({ extended: false }));
+// Parse incoming JSON data
+app.use(express.json());
+
+
+app.get("/api/notes", (req, res) => {
+
+    res.json(notes);
+
+});
+
+app.post("/api/notes", (req, res) => {
+
+    console.log(req.body);
+    res.json(req.body);
+
+});
+
 
 
 app.listen(3001, () => {
