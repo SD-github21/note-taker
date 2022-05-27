ebfb9abe9a67f0bcbbec750ba8c85685af19fe33
SD-github21 <sapandonde.gmail.com>
2022-05-26 23:20:55 -0500

    added uuid npm package to generate unique id and code to add unique id to notes

--- server.js
+++ server.js
@@ -6,13 +6,19 @@ const app = express();
 // const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
 const notes = require("./db/db.json");
-
+const { v4: uuidv4 } = require('uuid');
 
 // Add middleware to read posted/incoming data
 // Parse incoming string or array data
 app.use(express.urlencoded({ extended: false }));
 // Parse incoming JSON data
 app.use(express.json());
+
+function createNewNote(body, notesArray) {
+    console.log(body);
+
+    return(body);
+}
 
 
 app.get("/api/notes", (req, res) => {
@@ -22,8 +28,10 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.post("/api/notes", (req, res) => {
+    // Set ID using uuid npm package
+    req.body.id = uuidv4();
 
-    console.log(req.body);
+
     res.json(req.body);
 
 });
