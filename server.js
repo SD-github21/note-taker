109729c248c990eeaabf787495e05829d8c99b43
SD-github21 <sapandonde.gmail.com>
2022-05-27 23:07:36 -0500

    pull out html routes into a separate index.js file

--- server.js
+++ server.js
@@ -15,22 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
-
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-})
-
-
 app.listen(PORT, () => {
     console.log(`The API server is now on port ${PORT}!`);
 });
